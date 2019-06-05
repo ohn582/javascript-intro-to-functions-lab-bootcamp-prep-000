c3c2c65480009116943c552996425ab26c9b339d
Neal JIn Oh <no4259@gmail.com>
2019-06-05 04:37:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
   } else if (string.toLowerCase('HELLO') === string) {
     return 'I can\'t hear you!';
+  } else {
+    return 'I love you too';
   }
 }
 
