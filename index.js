3a296c20812d3c40768e40241ffcc1a7bdbc3b8b
Neal JIn Oh <no4259@gmail.com>
2019-06-05 04:31:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,56 @@
+function shout(string) {
+  return string.toUpperCase('hello');
+}
+console.log(shout('hello'));
+
+function whisper(string) {
+  return string.toLowerCase('HELLO');
+}
+console.log(whisper('HELLO'));
+
+
+
+function logShout(string) {
+  console.log('HELLO');
+}
+logShout()
+
+function logWhisper(string) {
+  console.log('hello');
+} 
+longWhisper()
+
+
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO";
+  var tolowercase = 'hello'
+  
+  if (uppercase.toUpperCase('hello') === uppercase) {
+    return 'I can\'t hear you!';
+  }
+  if (lowercase.toLowerCase('HELLO') === lowercase) {
+    return "YES INDEED!"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
